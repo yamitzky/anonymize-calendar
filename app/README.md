85ee550a6858c7ba2ddb642abc87538a8a04228b
Mitsuki Ogasahara <negiga@gmail.com>
2024-06-25 01:09:42 +0900

    Update to use service account

---
+++ app/README.md
@@ -0,0 +1,28 @@
+# Calendar Anonymization App
+
+This application is a tool for anonymizing calendar data in iCalendar format.
+
+## Features
+
+- Reading iCalendar files
+- Anonymizing event data
+- Outputting anonymized calendar data
+
+## Usage
+
+1. Start the application.
+2. Input the URL of the iCalendar file.
+3. The anonymized calendar data will be output.
+
+## Running the Server
+
+- To start the production server: `rye run server`
+- To start the development server: `rye run dev`
+
+## Dependencies
+
+This project uses [rye](https://rye.astral.sh/) for dependency management. Required dependencies are listed in the `pyproject.toml` file and locked in the `requirements.lock` file.
+
+## License
+
+For license information about this project, please refer to the LICENSE file in the root directory of the repository.
--- app/src/anonymize_calendar/app.py
+++ app/src/anonymize_calendar/app.py
@@ -1,12 +1,20 @@
 import datetime
-from typing import AsyncGenerator
+import os
 
-import httpx
+import google.auth
+import googleapiclient.discovery
 import icalendar
 import pytz
 from fastapi import FastAPI, Response
 
 app = FastAPI()
+
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+creds = google.auth.load_credentials_from_file(GOOGLE_APPLICATION_CREDENTIALS, SCOPES)[
+    0
+]
+service = googleapiclient.discovery.build("calendar", "v3", credentials=creds)
 
 
 @app.get("/")
@@ -21,22 +29,6 @@ async def robots():
     return Response("User-agent: *\nDisallow: /", media_type="text/plain")
 
 
-def is_older_than(a: datetime.datetime | datetime.date, b: datetime.date) -> bool:
-    """Check if a is older than b.
-
-    Args:
-        a: datetime object to compare.
-        b: date object to compare.
-
-    Returns:
-        True if a is older than b, False otherwise.
-    """
-
-    if isinstance(a, datetime.datetime):
-        return a.date() < b
-    return a < b
-
-
 def is_before_19pm_jst(dt: datetime.datetime) -> bool:
     """Check if the datetime object is before 19:00 JST.
 
@@ -46,73 +38,75 @@ def is_before_19pm_jst(dt: datetime.datetime) -> bool:
     Returns:
         True if the datetime is before 19:00 JST, False otherwise.
     """
-    japan_tz = pytz.timezone('Asia/Tokyo')
+    japan_tz = pytz.timezone("Asia/Tokyo")
     dt_jst = dt.astimezone(japan_tz)
     return dt_jst.hour < 19
 
 
-async def iter_events(url) -> AsyncGenerator[icalendar.Calendar, None]:
-    """Iterate over events in iCal file.
+@app.get("/calendar/{calendar_id}")
+async def calendar(calendar_id: str):
+    """Get anonymized future events from Google Calendar in iCalendar format.
 
     Args:
-        url: URL of iCal file.
-
-    Yields:
-        icalendar.Calendar: iCalendar event.
-    """
-    async with httpx.AsyncClient(timeout=None) as client:
-        async with client.stream("GET", url) as resp:
-            component_lines: list[str] = []
-            async for line in resp.aiter_lines():
-                if line.startswith("BEGIN:VEVENT"):
-                    component_lines = ["BEGIN:VEVENT"]
-                elif line.startswith("END:VEVENT"):
-                    component_lines.append("END:VEVENT")
-                    yield icalendar.Calendar.from_ical("\n".join(component_lines))
-                    component_lines = []
-                else:
-                    component_lines.append(line)
-
-
-@app.get("/calendar/{email}/{calendar_id}")
-async def calendar(email: str, calendar_id: str):
-    """Get anonymized iCal file.
-
-    Args:
-        email: User email address for Google Calendar.
         calendar_id: Calendar ID for Google Calendar.
     """
+    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+    events_result = (
+        service.events()
+        .list(
+            calendarId=calendar_id,
+            timeMin=now,
+            maxResults=1000,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events: list[dict] = events_result.get("items", [])
 
-    url = f"https://calendar.google.com/calendar/ical/{email}/{calendar_id}/basic.ics"
+    cal = icalendar.Calendar()
+    cal.add("prodid", f"-//My calendar product//{calendar_id}//")
+    cal.add("version", "2.0")
 
-    new_cal = icalendar.Calendar()
-    criteria = datetime.date.today() - datetime.timedelta(days=7)
+    for event in events:
+        start = event["start"].get("dateTime", event["start"].get("date"))
+        end = event["end"].get("dateTime", event["end"].get("date"))
+        summary = event.get("summary", "No Title")
 
-    # イベントを追加
-    async for component in iter_events(url):
-        start: datetime.datetime | datetime.date = component.get("dtstart").dt
-        summary: str | None = component.get("summary")
+        start_dt = datetime.datetime.fromisoformat(start.rstrip("Z"))
 
-        if is_older_than(start, criteria):
+        if "T" not in start:  # Skip all date events
             continue
-        if summary and "作業" in summary:
+        if "作業" in summary:
             continue
-        if summary:
-            if '面談' in summary or '面接' in summary:
-                summary = '会議'
-            elif isinstance(start, datetime.datetime) and is_before_19pm_jst(start):
-                summary = '会議'
-            else:
-                summary = '予定'
+        if "面談" in summary or "面接" in summary:
+            summary = "会議"
+        elif is_before_19pm_jst(start_dt):
+            summary = "会議"
+        elif "移動" in summary:
+            summary = "移動"
+        else:
+            summary = "予定"
 
         cal_event = icalendar.Event()
         cal_event.add("summary", summary)
-        cal_event.add("dtstart", start)
-        if end := component.get("dtend"):
-            cal_event.add("dtend", end.dt)
-        new_cal.add_component(cal_event)
 
-    ics_data = new_cal.to_ical()
+        if "T" in start:  # This is a date-time
+            cal_event.add("dtstart", start_dt)
+        else:  # This is a date
+            cal_event.add("dtstart", datetime.date.fromisoformat(start))
+
+        if "T" in end:  # This is a date-time
+            cal_event.add("dtend", datetime.datetime.fromisoformat(end.rstrip("Z")))
+        else:  # This is a date
+            cal_event.add("dtend", datetime.date.fromisoformat(end))
+
+        cal_event.add("dtstamp", datetime.datetime.utcnow())
+        cal_event["uid"] = event["id"]
+
+        cal.add_component(cal_event)
+
+    ics_data = cal.to_ical()
 
     headers = {"Content-Disposition": 'attachment; filename="calendar.ics"'}
-    return Response(content=ics_data, headers=headers)
+    return Response(content=ics_data, headers=headers, media_type="text/calendar")
